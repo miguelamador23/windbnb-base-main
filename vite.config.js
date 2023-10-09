1c06d99b31664a02cbd17c80e3eebb5ddf0078d0
miguelamador23 <143641330+miguelamador23@users.noreply.github.com>
2023-10-09 13:57:13 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://miguelamador23.github.io/windbnb-base/"
+  base: "/windbnb-base/"
 })
